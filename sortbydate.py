3187d81d0eb1d7feaa73510ec39d85c3b4421ca1
AkasakaID <fawwazthoerif@gmail.com>
2022-12-19 10:57:20 +0700

    update sortbydate.py & delete exe file

--- sortbydate.py
+++ sortbydate.py
@@ -5,24 +5,29 @@ from datetime import datetime
 
 
 def get_date_file(file):
-    stamp = os.path.getctime(file)
+    stamp = os.path.getmtime(file)
     datee = datetime.fromtimestamp(stamp)
-    _folder = f"{datee.day}-{datee.month}-{datee.year}"
+    bulan = str(datee.month).zfill(2)
+    hari = str(datee.day).zfill(2)
+    _folder = f"{datee.year}-{bulan}-{hari}"
     return _folder
 
 
 def main():
     list_files = os.listdir()
     for file in list_files:
-        if os.path.isdir(file) or file == "sortbydate.exe" or file == "sortbydate.py":
+        if os.path.isdir(file):
             continue
         else:
-            _folder = get_date_file(file)
-            if not os.path.exists(_folder):
-                os.makedirs(_folder)
-            dest_path = _folder + "/" + file
-            status = shutil.move(src=file, dst=dest_path)
-            print(f"moving {file} to {status}")
+            try:
+                _folder = get_date_file(file)
+                if not os.path.exists(_folder):
+                    os.makedirs(_folder)
+                dest_path = _folder + "/" + file
+                status = shutil.move(src=file, dst=dest_path)
+                print(f"moving {file} to {status}")
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
